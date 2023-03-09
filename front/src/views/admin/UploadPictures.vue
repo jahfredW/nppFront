54436e9c712d5f2cdc93a085f50bdcdcf9a0c438
fred.gruwe <fred.gruwe@gmail.com>
2023-03-09 10:26:28 +0100

    update UploadPictures.vue - ajout du système de validation de la taille

--- front/src/views/admin/UploadPictures.vue
+++ front/src/views/admin/UploadPictures.vue
@@ -32,6 +32,8 @@ export default {
     uploadFiles(){
       // définition de l'autorisation d'envoie à true
       let sendOk = true;
+      let successUpload = [];
+      let failureUpload = [];
       // tableau contenant les extensions possibles 
       const extension_tab = ['jpg', 'png', 'jpeg', 'gif', 'svg'];
       // instanciation d'un nouveau formulaire de payload avec l'objet FormData. 
@@ -41,48 +43,64 @@ export default {
       for( let image of this.files) {
         // on essaie de spliter le nom de l'image, pour savoir si elle contient au moins un point  
         try{
+          console.log(image);
           let splitImage = image.name.split(".");
+          let imageSize = image.size;
           let extension = splitImage[splitImage.length - 1];
           // Si l'image a un point mais que la dernier élément du split n'appartient pas à la liste des extensions, on refuse l'envoie. 
           if (!extension_tab.includes(extension)){
             alert('Au moins une image contient uen extension inconnue')
             sendOk = false;
+            failureUpload.push(image);
+            // break;
+          }
+
+          if (imageSize > 2000000){
+            alert(`Désolé, mais l\'image ${image.name} dépasse la taille autorisée `)
+            sendOk = false;
+            failureUpload.push(image);
             break;
           }
+          
+          
         }
         // Si l'image name ne contient aucun point, alors le format est inconnu, on refuse l'envoie
         catch {
           alert("format inconnu")
+          failureUpload.push(image);
           sendOk = false;
           break;
         }
-        
+      
+        successUpload.push(image);
       }
-    // Si le conteneur d'image est vide ou si l'autorisation d'envoie n'est pas accordée
-    // on alerte l'utlisateur 
-    if (this.files.length === 0  || !sendOk){
-      alert("aucune image à envoyer ou extension invalide");
-      // sinon on construit l'objet formData avec les uploads et on envoie sur le backend. 
-    } else {
-      for (let i = 0; i < this.files.length; i++) {
-        formData.append('files[]', this.files[i]);
-        
+      console.log(successUpload);
+      console.log(failureUpload);
+      // Si le conteneur d'image est vide ou si l'autorisation d'envoie n'est pas accordée
+      // on alerte l'utlisateur 
+      if (this.files.length === 0  || !sendOk){
+        alert("aucune image à envoyer ou extension invalide");
+        // sinon on construit l'objet formData avec les uploads et on envoie sur le backend. 
+      } else {
+        for (let i = 0; i < this.files.length; i++) {
+          formData.append('files[]', this.files[i]);
+          
+        }
+        accountService.uploadPictures(formData)
+        .then(
+          response => {
+            this.files = [];
+            this.$router.go(0);
+            alert('Les fichiers ont été uploadés avec succès')
+        })
+        .catch(
+          error => {
+          alert('Une erreur est survenue durant l\'envoie des fichiers')
+        });
       }
-      accountService.uploadPictures(formData)
-      .then(
-        response => {
-          this.files = [];
-          this.$router.go(0);
-          alert('Les fichiers ont été uploadés avec succès')
-      })
-      .catch(
-        error => {
-        alert('Une erreur est survenue durant l\'envoie des fichiers')
-      });
+      
+      }
     }
-    
-    }
-  }
 }
 </script>
 
